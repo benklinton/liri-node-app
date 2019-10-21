2111efef441f53ea5102eea458651ed02175e561
benklinton <benklinton@gmail.com>
2019-10-21 10:17:50 -0600

    created AJAX call for Spotify API

--- liri.js
+++ liri.js
@@ -1,3 +1,17 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify);
+// console.log(spotify);
+
+var command = process.argv[2]
+var input = process.argv[3]
+
+if (command === "spotify-this-song") {
+    spotify.search({type: "track", query: input,}, function(err, response) {
+        if(err) {
+            return console.log("error has occured " + err)
+        }
+        console.log(response.tracks.items[0].album)
+    })
+}
